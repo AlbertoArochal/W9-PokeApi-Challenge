d49db12ca1faa355b19fdefa43b7cd2219a0728f
AlbertoArochal <arochaldev@gmail.com>
2022-12-04 19:27:57 +0100

    added fixes to app.js

---
+++ src/app/app.test.ts
@@ -0,0 +1,24 @@
+test('app adds 25 li elements to the ul element', () => {
+    expect.assertions(1);
+    const mockLocalStorage = {
+        getItem: jest.fn(() =>
+            JSON.stringify([
+                {
+                    name: 'pikachu',
+                    url: 'https://pokeapi.co/api/v2/pokemon/25',
+                },
+            ])
+        ),
+    };
+    const mockDocument = {
+        querySelector: jest.fn(() => ({
+            innerHTML: '',
+        })),
+    };
+
+    app(mockLocalStorage, mockDocument);
+
+    expect(mockDocument.querySelector().innerHTML).toBe(
+        '<li> Pokemon Name: pikachu; url = https://pokeapi.co/api/v2/pokemon/25 </li>'
+    );
+});
